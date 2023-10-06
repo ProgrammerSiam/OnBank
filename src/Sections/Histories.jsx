de84cbb98925e2de20959739399c59ddbc2aeb2f
ProgrammerSiam <programmersiam.com@gmail.com>
2023-10-07 03:12:53 +0600

    one spache in histories section

--- src/Sections/Histories.jsx
+++ src/Sections/Histories.jsx
@@ -4,7 +4,7 @@ export default function Histories() {
       <div className="container px-20 text-center">
         <h2 className="mb-5 text-3xl font-semibold">
           Money Management Made Easy Make Up
-          <span className="text-purple">40%</span> Lorem ipsum dolor sit amet
+          <span className="text-purple"> 40%</span> Lorem ipsum dolor sit amet
           consectetur adipisicing elit. Fuga quae perferendis ipsum quo eveniet
           a quaerat mollitia reprehenderit cum voluptate!
         </h2>
